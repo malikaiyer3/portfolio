085dd509eee6d2f433bee07b881de657b6ce9f7a
malikaiyer <malikakarthik@gmail.com>
2024-01-15 12:43:31 +0100

    changed background color

--- src/App.tsx
+++ src/App.tsx
@@ -16,15 +16,11 @@ function App(): JSX.Element {
             </header>
             <div className="content">
                 <section id="scrollspyHeading1">
-                    <h4>First Heading</h4>
+                    <h4>About Me</h4>
                     <p>
                         At vero eos et accusamus et iusto odio dignissimos
                         ducimus qui blanditiis praesentium voluptatum deleniti
                         atque corrupti quos dolores et quas molestias excepturi
-                        sint occaecati cupiditate non provident, similique sunt
-                        in culpa qui officia deserunt mollitia animi, id est
-                        laborum et dolorum fuga. Et harum quidem rerum facilis
-                        est et expedita distinctio. Nam libero tempore, cum
                         soluta nobis est eligendi optio cumque nihil impedit quo
                         minus id quod maxime placeat facere possimus, omnis
                         voluptas assumenda est, omnis dolor repellendus.
@@ -37,93 +33,17 @@ function App(): JSX.Element {
                         accusamus et iusto odio dignissimos ducimus qui
                         blanditiis praesentium voluptatum deleniti atque
                         corrupti quos dolores et quas molestias excepturi sint
-                        occaecati cupiditate non provident, similique sunt in
-                        culpa qui officia deserunt mollitia animi, id est
-                        laborum et dolorum fuga. Et harum quidem rerum facilis
-                        est et expedita distinctio. Nam libero tempore, cum
-                        soluta nobis est eligendi optio cumque nihil impedit quo
-                        minus id quod maxime placeat facere possimus, omnis
-                        voluptas assumenda est, omnis dolor repellendus.
-                        Temporibus autem quibusdam et aut officiis debitis aut
-                        rerum necessitatibus saepe eveniet ut et voluptates
-                        repudiandae sint et molestiae non recusandae. Itaque
-                        earum rerum hic tenetur a sapiente delectus, ut aut
-                        reiciendis voluptatibus maiores alias consequatur aut
-                        perferendis doloribus asperiores repellat At vero eos et
-                        accusamus et iusto odio dignissimos ducimus qui
-                        blanditiis praesentium voluptatum deleniti atque
-                        corrupti quos dolores et quas molestias excepturi sint
-                        occaecati cupiditate non provident, similique sunt in
-                        culpa qui officia deserunt mollitia animi, id est
-                        laborum et dolorum fuga. Et harum quidem rerum facilis
-                        est et expedita distinctio. Nam libero tempore, cum
-                        soluta nobis est eligendi optio cumque nihil impedit quo
-                        minus id quod maxime placeat facere possimus, omnis
-                        voluptas assumenda est, omnis dolor repellendus.
-                        Temporibus autem quibusdam et aut officiis debitis aut
-                        rerum necessitatibus saepe eveniet ut et voluptates
-                        repudiandae sint et molestiae non recusandae. Itaque
-                        earum rerum hic tenetur a sapiente delectus, ut aut
-                        reiciendis voluptatibus maiores alias consequatur aut
-                        perferendis doloribus asperiores repellat At vero eos et
-                        accusamus et iusto odio dignissimos ducimus qui
-                        blanditiis praesentium voluptatum deleniti atque
-                        corrupti quos dolores et quas molestias excepturi sint
-                        occaecati cupiditate non provident, similique sunt in
-                        culpa qui officia deserunt mollitia animi, id est
-                        laborum et dolorum fuga. Et harum quidem rerum facilis
-                        est et expedita distinctio. Nam libero tempore, cum
-                        soluta nobis est eligendi optio cumque nihil impedit quo
-                        minus id quod maxime placeat facere possimus, omnis
-                        voluptas assumenda est, omnis dolor repellendus.
-                        Temporibus autem quibusdam et aut officiis debitis aut
-                        rerum necessitatibus saepe eveniet ut et voluptates
-                        repudiandae sint et molestiae non recusandae. Itaque
-                        earum rerum hic tenetur a sapiente delectus, ut aut
-                        reiciendis voluptatibus maiores alias consequatur aut
-                        perferendis doloribus asperiores repellat
+                        occaecati cupiditate non provident, similique sunt i
                     </p>
                 </section>
                 <section id="scrollspyHeading2">
-                    <h4>Second Heading</h4>
+                    <h4>Skills</h4>
                     <p>
                         At vero eos et accusamus et iusto odio dignissimos
                         ducimus qui blanditiis praesentium voluptatum deleniti
                         atque corrupti quos dolores et quas molestias excepturi
                         sint occaecati cupiditate non provident, similique sunt
                         in culpa qui officia deserunt mollitia animi, id est
-                        laborum et dolorum fuga. Et harum quidem rerum facilis
-                        est et expedita distinctio. Nam libero tempore, cum
-                        soluta nobis est eligendi optio cumque nihil impedit quo
-                        minus id quod maxime placeat facere possimus, omnis
-                        voluptas assumenda est, omnis dolor repellendus.
-                        Temporibus autem quibusdam et aut officiis debitis aut
-                        rerum necessitatibus saepe eveniet ut et voluptates
-                        repudiandae sint et molestiae non recusandae. Itaque
-                        earum rerum hic tenetur a sapiente delectus, ut aut
-                        reiciendis voluptatibus maiores alias consequatur aut
-                        perferendis doloribus asperiores repellat At vero eos et
-                        accusamus et iusto odio dignissimos ducimus qui
-                        blanditiis praesentium voluptatum deleniti atque
-                        corrupti quos dolores et quas molestias excepturi sint
-                        occaecati cupiditate non provident, similique sunt in
-                        culpa qui officia deserunt mollitia animi, id est
-                        laborum et dolorum fuga. Et harum quidem rerum facilis
-                        est et expedita distinctio. Nam libero tempore, cum
-                        soluta nobis est eligendi optio cumque nihil impedit quo
-                        minus id quod maxime placeat facere possimus, omnis
-                        voluptas assumenda est, omnis dolor repellendus.
-                        Temporibus autem quibusdam et aut officiis debitis aut
-                        rerum necessitatibus saepe eveniet ut et voluptates
-                        repudiandae sint et molestiae non recusandae. Itaque
-                        earum rerum hic tenetur a sapiente delectus, ut aut
-                        reiciendis voluptatibus maiores alias consequatur aut
-                        perferendis doloribus asperiores repellat At vero eos et
-                        accusamus et iusto odio dignissimos ducimus qui
-                        blanditiis praesentium voluptatum deleniti atque
-                        corrupti quos dolores et quas molestias excepturi sint
-                        occaecati cupiditate non provident, similique sunt in
-                        culpa qui officia deserunt mollitia animi, id est
                         laborum et dolorum fuga. Et harum quidem rerum facilis
                         est et expedita distinctio. Nam libero tempore, cum
                         soluta nobis est eligendi optio cumque nihil impedit quo
@@ -138,7 +58,7 @@ function App(): JSX.Element {
                     </p>
                 </section>
                 <section id="scrollspyHeading3">
-                    <h4>Third Heading</h4>
+                    <h4>Projects</h4>
                     <p>
                         At vero eos et accusamus et iusto odio dignissimos
                         ducimus qui blanditiis praesentium voluptatum deleniti
@@ -148,50 +68,14 @@ function App(): JSX.Element {
                         laborum et dolorum fuga. Et harum quidem rerum facilis
                         est et expedita distinctio. Nam libero tempore, cum
                         soluta nobis est eligendi optio cumque nihil impedit quo
-                        minus id quod maxime placeat facere possimus, omnis
-                        voluptas assumenda est, omnis dolor repellendus.
-                        Temporibus autem quibusdam et aut officiis debitis aut
-                        rerum necessitatibus saepe eveniet ut et voluptates
-                        repudiandae sint et molestiae non recusandae. Itaque
-                        earum rerum hic tenetur a sapiente delectus, ut aut
-                        reiciendis voluptatibus maiores alias consequatur aut
-                        perferendis doloribus asperiores repellat At vero eos et
-                        accusamus et iusto odio dignissimos ducimus qui
-                        blanditiis praesentium voluptatum deleniti atque
-                        corrupti quos dolores et quas molestias excepturi sint
-                        occaecati cupiditate non provident, similique sunt in
-                        culpa qui officia deserunt mollitia animi, id est
-                        laborum et dolorum fuga. Et harum quidem rerum facilis
-                        est et expedita distinctio. Nam libero tempore, cum
                         soluta nobis est eligendi optio cumque nihil impedit quo
                         minus id quod maxime placeat facere possimus, omnis
                         voluptas assumenda est, omnis dolor repellendus.
                         Temporibus autem quibusdam et aut officiis debitis aut
-                        rerum necessitatibus saepe eveniet ut et voluptates
-                        repudiandae sint et molestiae non recusandae. Itaque
-                        earum rerum hic tenetur a sapiente delectus, ut aut
-                        reiciendis voluptatibus maiores alias consequatur aut
-                        perferendis doloribus asperiores repellat At vero eos et
-                        accusamus et iusto odio dignissimos ducimus qui
-                        blanditiis praesentium voluptatum deleniti atque
-                        corrupti quos dolores et quas molestias excepturi sint
-                        occaecati cupiditate non provident, similique sunt in
-                        culpa qui officia deserunt mollitia animi, id est
-                        laborum et dolorum fuga. Et harum quidem rerum facilis
-                        est et expedita distinctio. Nam libero tempore, cum
-                        soluta nobis est eligendi optio cumque nihil impedit quo
-                        minus id quod maxime placeat facere possimus, omnis
-                        voluptas assumenda est, omnis dolor repellendus.
-                        Temporibus autem quibusdam et aut officiis debitis aut
-                        rerum necessitatibus saepe eveniet ut et voluptates
-                        repudiandae sint et molestiae non recusandae. Itaque
-                        earum rerum hic tenetur a sapiente delectus, ut aut
-                        reiciendis voluptatibus maiores alias consequatur aut
-                        perferendis doloribus asperiores repellat
                     </p>
                 </section>
                 <section id="scrollspyHeading4">
-                    <h4>Fourth Heading</h4>
+                    <h4>Links</h4>
                     <p>
                         At vero eos et accusamus et iusto odio dignissimos
                         ducimus qui blanditiis praesentium voluptatum deleniti
@@ -202,45 +86,6 @@ function App(): JSX.Element {
                         est et expedita distinctio. Nam libero tempore, cum
                         soluta nobis est eligendi optio cumque nihil impedit quo
                         minus id quod maxime placeat facere possimus, omnis
-                        voluptas assumenda est, omnis dolor repellendus.
-                        Temporibus autem quibusdam et aut officiis debitis aut
-                        rerum necessitatibus saepe eveniet ut et voluptates
-                        repudiandae sint et molestiae non recusandae. Itaque
-                        earum rerum hic tenetur a sapiente delectus, ut aut
-                        reiciendis voluptatibus maiores alias consequatur aut
-                        perferendis doloribus asperiores repellat At vero eos et
-                        accusamus et iusto odio dignissimos ducimus qui
-                        blanditiis praesentium voluptatum deleniti atque
-                        corrupti quos dolores et quas molestias excepturi sint
-                        occaecati cupiditate non provident, similique sunt in
-                        culpa qui officia deserunt mollitia animi, id est
-                        laborum et dolorum fuga. Et harum quidem rerum facilis
-                        est et expedita distinctio. Nam libero tempore, cum
-                        soluta nobis est eligendi optio cumque nihil impedit quo
-                        minus id quod maxime placeat facere possimus, omnis
-                        voluptas assumenda est, omnis dolor repellendus.
-                        Temporibus autem quibusdam et aut officiis debitis aut
-                        rerum necessitatibus saepe eveniet ut et voluptates
-                        repudiandae sint et molestiae non recusandae. Itaque
-                        earum rerum hic tenetur a sapiente delectus, ut aut
-                        reiciendis voluptatibus maiores alias consequatur aut
-                        perferendis doloribus asperiores repellat At vero eos et
-                        accusamus et iusto odio dignissimos ducimus qui
-                        blanditiis praesentium voluptatum deleniti atque
-                        corrupti quos dolores et quas molestias excepturi sint
-                        occaecati cupiditate non provident, similique sunt in
-                        culpa qui officia deserunt mollitia animi, id est
-                        laborum et dolorum fuga. Et harum quidem rerum facilis
-                        est et expedita distinctio. Nam libero tempore, cum
-                        soluta nobis est eligendi optio cumque nihil impedit quo
-                        minus id quod maxime placeat facere possimus, omnis
-                        voluptas assumenda est, omnis dolor repellendus.
-                        Temporibus autem quibusdam et aut officiis debitis aut
-                        rerum necessitatibus saepe eveniet ut et voluptates
-                        repudiandae sint et molestiae non recusandae. Itaque
-                        earum rerum hic tenetur a sapiente delectus, ut aut
-                        reiciendis voluptatibus maiores alias consequatur aut
-                        perferendis doloribus asperiores repellat
                     </p>
                 </section>
             </div>
