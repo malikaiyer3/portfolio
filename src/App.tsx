14d3cfb53e08b3f34fca50a6adb2f7192072b053
malikaiyer <malikakarthik@gmail.com>
2024-06-01 09:27:44 +0530

    added projects template section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,28 @@ import "./App.css";
 import styled, { css, keyframes } from "styled-components";
 import { Link } from "react-scroll";
 import { useInView } from "react-intersection-observer";
+import pyIcon from "./icons8-python.svg";
+import gitIcon from "./icons8-git.svg";
+import javaIcon from "./icons8-java-50.png";
+import cIcon from "./icons8-c-50.png";
+import tsIcon from "./icons8-code-50.png";
+import kotIcon from "./icons8-kotlin.svg";
+import sqlIcon from "./icons8-database-administrator-50.png";
+
+const Header = styled.header`
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    background-color: #095f35;
+    min-height: 40vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(40px + 2vmin);
+    font-family: "Sobiscuit";
+    color: white;
+`;
 
 const HeaderComponent = styled.div`
     display: flex;
@@ -45,18 +67,23 @@ const NavLink = styled(Link)`
     }
 `;
 
-const AboutSection = styled.section<{ inView: boolean }>`
-    display: flex;
+const Section = styled.section<{ inView: boolean }>`
     padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0 0.125rem 0.25rem 0 rgba(0, 0, 0, 0.11);
-    font-size: 1.5 rem;
-    line-height: 1.5;
-    text-align: center;
-    margin-bottom: 20px;
-    margin-top: 20px;
+    opacity: 0;
+    transition: opacity 2s ease, transform 2s ease;
+    transform: translateY(20px);
+    ${({ inView }) =>
+        inView &&
+        css`
+            opacity: 1;
+            transform: translateY(0);
+        `}
+`;
 
-    ${({ inView }) => inView && fadeInAnimation}
+const AboutContainer = styled.p`
+    font-size: 1.2rem;
+    display: flex;
+    margin-bottom: 20px;
 `;
 
 const AboutImage = styled.img`
@@ -65,31 +92,9 @@ const AboutImage = styled.img`
     margin-left: 20px;
 `;
 
-const fadeIn = keyframes`
-  from {
-    opacity: 0;
-    transform: translateY(20px);
-  }
-  to {
-    opacity: 1;
-    transform: translateY(0);
-  }
-`;
-
-const fadeInAnimation = css`
-    opacity: 0;
-    animation: ${fadeIn} 1s forwards;
-`;
-
 /* Skills Section  */
 
 // Styled components for Skills section
-const SkillsSection = styled.section<{ inView: boolean }>`
-    padding: 20px;
-
-    ${({ inView }) => inView && fadeInAnimation}
-`;
-
 const BoxesContainer = styled.div`
     display: flex;
     flex-wrap: wrap;
@@ -107,29 +112,74 @@ const Box = styled.div`
 
     &:hover {
         background-color: var(--primary-color);
-        color: var(--white-color);
+        color: var(--white-color) !important;
     }
 `;
 
-const Icon = styled.div`
-    height: 50px;
-    width: 50px;
-    background-color: var(--primary-color);
-    border-radius: 50%;
-    text-align: center;
-    line-height: 50px;
-    font-size: 18px;
-    color: var(--white-color);
-    margin: 0 auto 10px auto;
+const Icon = styled.img`
+    width: 50px; /* Adjust the width as needed */
+    height: auto;
+    margin-bottom: 10px; /* Add some margin to separate the icon and text */
 `;
 
 const Topic = styled.div`
     color: var(--black-color);
-    font-size: 25px;
+    font-size: 18px; /* Reduce the font size for better alignment */
     font-weight: 500;
-    margin-bottom: 10px;
 `;
 /* Skills styling ends here */
+
+/* Projects section styling */
+const ProjectContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    text-align: left;
+    margin-bottom: 20px;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0.125rem 0.25rem 0 rgba(0, 0, 0, 0.11);
+`;
+
+const ProjectImage = styled.img`
+    width: 300px;
+    height: auto;
+    border-radius: 10px;
+    margin-bottom: 10px;
+`;
+
+const ProjectTitle = styled.h3`
+    text-align: left;
+    font-size: 1.5rem;
+    margin-bottom: 10px;
+`;
+
+const ProjectDescription = styled.p`
+    text-align: left;
+    font-size: 1rem;
+    margin-bottom: 10px;
+`;
+
+const ProjectLinks = styled.div`
+    display: flex;
+    gap: 10px;
+`;
+
+const ProjectLink = styled.a`
+    text-decoration: none;
+    color: #333;
+    font-weight: bold;
+    padding: 0.5rem 1rem;
+    border-radius: 5px;
+    background-color: #e0e0e0;
+    transition: background-color 0.3s;
+
+    &:hover {
+        background-color: #b36a5e;
+        color: white;
+    }
+`;
+/* Projects section styling ends here */
 
 function App(): JSX.Element {
     //animation code for each section
@@ -148,12 +198,12 @@ function App(): JSX.Element {
 
     //skills array
     const skills = [
-        { name: "HTML", icon: "fab fa-html5" },
-        { name: "CSS", icon: "fab fa-css3-alt" },
-        { name: "JavaScript", icon: "fab fa-js-square" },
-        { name: "Bootstrap", icon: "fab fa-bootstrap" },
-        { name: "React JS", icon: "fab fa-react" },
-        { name: "Git", icon: "fab fa-git-alt" }
+        { name: "Python", icon: pyIcon },
+        { name: "TS/HTML/CSS", icon: tsIcon },
+        { name: "Java", icon: javaIcon },
+        { name: "C++", icon: cIcon },
+        { name: "SQL", icon: sqlIcon },
+        { name: "Git", icon: gitIcon }
     ];
 
     return (
@@ -168,6 +218,7 @@ function App(): JSX.Element {
                                     smooth
                                     spy
                                     to="about"
+                                    duration={300}
                                 >
                                     About
                                 </NavLink>
@@ -178,8 +229,9 @@ function App(): JSX.Element {
                                     smooth
                                     spy
                                     to="skills"
+                                    duration={300}
                                 >
-                                    Navigation
+                                    Skills
                                 </NavLink>
                             </NavItem>
                             <NavItem>
@@ -188,8 +240,9 @@ function App(): JSX.Element {
                                     smooth
                                     spy
                                     to="projects"
+                                    duration={300}
                                 >
-                                    Connect
+                                    Projects
                                 </NavLink>
                             </NavItem>
                             <NavItem>
@@ -198,6 +251,7 @@ function App(): JSX.Element {
                                     smooth
                                     spy
                                     to="contact"
+                                    duration={300}
                                 >
                                     Contact Us
                                 </NavLink>
@@ -206,66 +260,75 @@ function App(): JSX.Element {
                     </nav>
                 </header>
             </HeaderComponent>
-            <header className="App-header">
+            <Header>
                 <div
                     className="animate__animated animate__bounceIn"
                     style={{ animationDuration: "2s" }}
                 >
                     Welcome to Malika Iyer&rsquo;s Personal Website!
                 </div>
-            </header>
-            <AboutSection id="about" ref={aboutRef} inView={aboutInView}>
-                Hello! I am Malika Iyer, a fourth year BS Computer Science
-                student at the University of Delaware. I am interested in the
-                fields of Human-Computer Interaction and Artificial
-                Intelligence. I have experience as a software engineering intern
-                at Mindsprint and as a research assistant at the Sensify Lab at
-                UD. I hope to work in software engineering in the future,
-                specifically on something that makes a real-world impact because
-                I am passionate about social change and human rights. In my free
-                time, I like to spend time with friends/family, do photography,
-                listen to music, sing, or play the ukulele! I am an avid dog and
-                cat lover but do not have any pets of my own (yet)!
-                <AboutImage
-                    src="https://avatars.githubusercontent.com/u/44524659?v=4"
-                    alt="Image"
-                />
-            </AboutSection>
-            {/*------------- Skills Section----------------- */}
-            <SkillsSection id="skills" ref={skillsRef} inView={skillsInView}>
-                <div className="content">
-                    <div className="title"></div>
-                    <BoxesContainer>
-                        {skills.map((skill, index) => (
-                            <Box key={index}>
-                                <Icon>
-                                    <i className={skill.icon}></i>
-                                </Icon>
-                                <Topic>{skill.name}</Topic>
-                            </Box>
-                        ))}
-                    </BoxesContainer>
-                </div>
-            </SkillsSection>
-
-            <section id="Projects" className="App-section">
+            </Header>
+            <Section id="about" ref={aboutRef} inView={aboutInView}>
+                <h2>About</h2>
+                <AboutContainer>
+                    Hello! I am Malika Iyer, a fourth year BS Computer Science
+                    student at the University of Delaware. I am interested in
+                    the fields of Human-Computer Interaction and Artificial
+                    Intelligence. I have experience as a software engineering
+                    intern at Mindsprint and as a research assistant at the
+                    Sensify Lab at UD. I hope to work in software engineering in
+                    the future, specifically on something that makes a
+                    real-world impact because I am passionate about social
+                    change and human rights. In my free time, I like to spend
+                    time with friends/family, do photography, listen to music,
+                    sing, or play the ukulele! I am an avid dog and cat lover
+                    but do not have any pets of my own (yet)!
+                    <AboutImage
+                        src="https://avatars.githubusercontent.com/u/44524659?v=4"
+                        alt="Image"
+                    />
+                </AboutContainer>
+            </Section>
+            <Section id="skills" ref={skillsRef} inView={skillsInView}>
+                <h2>Skills</h2>
+                <BoxesContainer>
+                    {skills.map((skill, index) => (
+                        <Box key={index}>
+                            <Icon src={skill.icon} alt="Image" />
+                            <Topic>{skill.name}</Topic>
+                        </Box>
+                    ))}
+                </BoxesContainer>
+            </Section>
+            <Section id="projects" ref={projectsRef} inView={projectsInView}>
                 <h2>Projects</h2>
-                Lorem ipsum
-            </section>
-            <AboutSection id="contact" ref={contactRef} inView={contactInView}>
+                <ProjectContainer>
+                    <ProjectImage src={ProjectImage} alt="Project Image" />
+                    <ProjectTitle>Project Title</ProjectTitle>
+                    <ProjectDescription>
+                        This is a short description of the project. It
+                        highlights the main features and functionalities.
+                    </ProjectDescription>
+                    <ProjectLinks>
+                        <ProjectLink
+                            href="https://github.com/yourusername/yourproject"
+                            target="_blank"
+                        >
+                            GitHub
+                        </ProjectLink>
+                        <ProjectLink
+                            href="https://yourproject.com"
+                            target="_blank"
+                        >
+                            Live Project
+                        </ProjectLink>
+                    </ProjectLinks>
+                </ProjectContainer>
+            </Section>
+            <Section id="contact" ref={contactRef} inView={contactInView}>
                 <h2>Contact</h2>
-                Hello! I am Malika Iyer, a fourth year BS Computer Science
-                student at the University of Delaware. I am interested in the
-                fields of Human-Computer Interaction and Artificial
-                Intelligence. I have experience as a software engineering intern
-                at Mindsprint and as a research assistant at the Sensify Lab at
-                UD. I hope to work in software engineering in the future,
-                specifically on something that makes a real-world impact because
-                I am passionate about social change and human rights. In my free
-                time, I like to spend time with friends/family, do photography,
-                listen to music, sing, or play the ukulele! I am an avid dog and
-                cat lover but do not have any pets of my own (yet)!
-            </AboutSection>
+                <p>Email: malika@udel.edu</p>
+            </Section>
         </div>
     );
 }
